969982a5fb292c7f87b6286d36e2187acc183d1b
Andrei Kondratev <andreik@catalyst.net.nz>
2015-05-26 17:12:23 +1200

    WIP

--- README.md
+++ README.md
@@ -1,1 +1,23 @@
-# moment-weekday-calc
+# Moment Weekday Calculator plugin
+This plugin will count specific weekdays in the range.
+
+# Syntax
+Calculate specified weekdays for dates range: 
+```moment().weekdaycalc(rangeStart moment|Date|String, rangeEnd moment|Date|String, weekdays Array);```
+
+Calculate specified weekdays for dates range: 
+```moment(rangeStart moment|Date|String).weekdaycalc(rangeEnd moment|Date|String, weekdays Array);```
+
+Calculate specified weekdays for the whole current calendar year: 
+```moment().weekdaycalc(weekdays Array);```
+
+**rangeStart** - the range start
+
+**rangeEnd** - the range end
+
+**weekdays** - array of locale aware weekday numbers for example [1, 2, 3]
+please notice that it may be 1-7 or 0-6 depending on locale
+
+
+
+# Usage
--- test/weekdaycalc.js
+++ test/weekdaycalc.js
@@ -1,12 +1,24 @@
 if ( typeof require !== 'undefined' )
 {
   moment = require('moment');
-  expect = require("chai").expect
+  expect = require("chai").expect;
   require('../moment.weekdaycalc');
 }
 
 describe('Sanity check', function(){
-  it('Check that plugin is attached to the momentjs',function(){
+  it('plugin is attached to the momentjs',function(){
     expect(moment().weekdaycalc).to.a('function');
+  });
+});
+
+describe('Syntax check', function(){
+  it('3 args',function(){
+    expect(moment().weekdaycalc([2015,0,1],[2015,11,31],[1,2,3,4,5])).to.equal(261);
+  });
+  it('2 args',function(){
+    expect(moment([2015,0,1]).weekdaycalc([2015,11,31],[1,2,3,4,5])).to.equal(261);
+  });
+  it('1 arg',function(){
+    expect(moment([2015,0,1]).weekdaycalc([1,2,3,4,5])).to.equal(261);
   });
 });
