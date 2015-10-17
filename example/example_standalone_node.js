1a2bae089562cdf15c89a9701d4904579c5d2985
Andrew Kondratev <andruhon@gmail.com>
2015-10-17 14:46:56 +1300

    Fix #4 'standalone' node.js demo

--- example/example_standalone_node.js
+++ example/example_standalone_node.js
@@ -3,5 +3,6 @@
 WeekDayCalc = require('moment-weekday-calc');
 
 var useIsoWeekday = true;
-var calc = new WeekDayCalc([2015,0,1],[2015,11,31],[1,2,3,4,5,6,7],useIsoWeekday);
+var exclusions = [[2015,3,6],[2015,3,7]];
+var calc = new WeekDayCalc([2015,0,1],[2015,11,31],[1,2,3,4,5,6,7], exclusions, useIsoWeekday);
 console.log(calc.calculate());
